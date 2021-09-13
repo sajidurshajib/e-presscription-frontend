525579f543a106b3183f9b1e2e72b902d67d3e56
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-09-13 10:20:51 +0600

    added HomePage RegisterPage LoginPage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import { Suspense } from 'react'
+import Dashboard from '../components/Dashboard/Dashboard'
+const HomePage = () => {
+    return (
+        <div>
+            {/* added lazy loading */}
+            <Suspense fallback={<div>Loading...</div>}>
+                <Dashboard />
+            </Suspense>
+        </div>
+    )
+}
+
+export default HomePage
