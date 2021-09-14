6d7f3bf0dcd61b58d3fe6e25e40b7be9e2995f8d
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-09-14 17:41:27 +0600

    Dgrid added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,17 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import classes from './Dashboard.module.css'
+import Sidebar from './Sidebar/Sidebar'
+
 const Dashboard = () => {
     return (
-        <div>
-            <h2>Dashboard</h2>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
+        <div className={`${classes.Dashboard}`}>
+            {/* <div className={`${classes.Dashboard} ${classes.minimize}`}> */}
+            <Sidebar />
+            <div>
+                {/* <h2>Dashboard</h2>
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link> */}
+            </div>
         </div>
     )
 }
