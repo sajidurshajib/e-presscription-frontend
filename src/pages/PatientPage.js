f9409a0c87de4a2ee5d30f5fdfb1f7551e627cff
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-09-14 17:36:16 +0600

    patient page added

---
+++ src/pages/PatientPage.js
@@ -0,0 +1,11 @@
+import Patient from '../components/Patient/Patient'
+
+const PatientPage = () => {
+    return (
+        <div>
+            <Patient />
+        </div>
+    )
+}
+
+export default PatientPage
