6796198c8d69926946b5dbddcec75082c8e74cf2
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-09-14 17:38:36 +0600

    header component added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import classes from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div className={classes.Header}>
+            <div>Doctor</div>
+            <div>Patient</div>
+        </div>
+    )
+}
+
+export default Header
