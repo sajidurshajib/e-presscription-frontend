241f0739b57d776fa9e4584eed8d03a4201725ae
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-09-16 17:30:03 +0600

    history preview style completed

--- src/components/PreviewSection/HistoryPrev/HistoryPrev.js
+++ src/components/PreviewSection/HistoryPrev/HistoryPrev.js
@@ -1,53 +1,29 @@
+import { useState } from 'react'
 import classes from './HistoryPrev.module.css'
+import CoMorbidity from './HistoryPrevChild/CoMorbiditry'
+import Drug from './HistoryPrevChild/Drug'
+import Family from './HistoryPrevChild/Family'
+import Medical from './HistoryPrevChild/Medical'
+import Personal from './HistoryPrevChild/Personal'
+import Professional from './HistoryPrevChild/Professional'
 
 const HistoryPrev = () => {
+    const [toggle, setToggle] = useState(true)
     return (
         <div className={classes.HistoryPrev}>
-            <h3>History</h3>
-            <h4>Personal history</h4>
-            <ul className={classes.historyli}>
-                <li>Demo exist</li>
-                <li>Demo problem</li>
-                <li>Demo exist</li>
-            </ul>
-            <h4>Professional history</h4>
-            <ul className={classes.historyli}>
-                <li>Demo exist</li>
-                <li>Demo problem</li>
-            </ul>
-            <h4>Family history</h4>
-            <ul className={classes.historyli}>
-                <li>Demo exist</li>
-                <li>Demo problem</li>
-                <li>Demo exist</li>
-            </ul>
-            <h4>Drug history</h4>
-            <ul className={classes.historyli}>
-                <li>Demo exist</li>
-                <li>Demo problem</li>
-            </ul>
-            <h4>Medical history</h4>
-            <ul className={classes.historyli}>
-                <li>Demo exist</li>
-                <li>Demo problem</li>
-                <li>Demo exist</li>
-                <li>Demo problem</li>
-            </ul>
-            <h3>Co-Morbidity</h3>
-            <ul className={classes.combrt}>
-                <li>
-                    Hypertension (HTN)<span className={classes.red}>+</span>
-                </li>
-                <li>
-                    Diabetes (DM)<span>-</span>
-                </li>
-                <li>
-                    Tuberculosis (TB)<span className={classes.red}>+</span>
-                </li>
-                <li>
-                    Asthma <span>-</span>
-                </li>
-            </ul>
+            <h3 onClick={() => setToggle(!toggle)} className={toggle ? classes.toggle : null}>
+                History
+            </h3>
+            {toggle ? (
+                <>
+                    <Personal />
+                    <Professional />
+                    <Family />
+                    <Drug />
+                    <Medical />
+                    <CoMorbidity />
+                </>
+            ) : null}
         </div>
     )
 }
