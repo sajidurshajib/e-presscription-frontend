4fdfb98b0f731dedb5f71705aaa49488617d1e59
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-09-16 17:31:16 +0600

    Investigation preview style completed

--- src/components/PreviewSection/InvestigationPrev/InvestigationPrev.js
+++ src/components/PreviewSection/InvestigationPrev/InvestigationPrev.js
@@ -1,18 +1,24 @@
+import { useState } from 'react'
 import classes from './InvestigationPrev.module.css'
 
 const InvestigationPrev = () => {
+    const [toggle, setToggle] = useState(true)
+
     return (
         <div className={classes.InvestigationPrev}>
-            <h3>Investigation</h3>
-            <ul>
-                <li>CVC</li>
-                <li>X-Ray</li>
-                <li>CSR</li>
-                <li>MRI</li>
-                <li>CTScan</li>
-                <li>Urine R/E</li>
-                <li>Urine C/S</li>
-            </ul>
+            <h3 onClick={() => setToggle(!toggle)} className={toggle ? classes.toggle : null}>
+                Investigation
+            </h3>
+            {toggle ? (
+                <>
+                    <ul>
+                        <li>CVC</li>
+                        <li>X-Ray</li>
+                        <li>Urine R/E</li>
+                        <li>Urine C/S</li>
+                    </ul>
+                </>
+            ) : null}
         </div>
     )
 }
