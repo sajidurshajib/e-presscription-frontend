affcf61281dd5d9bf8575396c1d6ad5626a34816
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-09-16 17:27:59 +0600

    Diagnosis preview style complete

--- src/components/PreviewSection/DiagnosisPrev/DiagnosisPrev.js
+++ src/components/PreviewSection/DiagnosisPrev/DiagnosisPrev.js
@@ -1,4 +1,36 @@
+import { useState } from 'react'
+import classes from './DiagnosisPrev.module.css'
+
 const DiagnosisPrev = () => {
-    return <div></div>
+    const [toggle, setToggle] = useState(true)
+
+    return (
+        <div className={classes.DiagnosisPrev}>
+            <h3 onClick={() => setToggle(!toggle)} className={toggle ? classes.toggle : null}>
+                Diagnosis
+            </h3>
+            {toggle ? (
+                <>
+                    <div className={classes.diagnosis}>
+                        <div className={classes.probable}>
+                            <h4>Probable</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iure sit nam fuga ullam.
+                                Autem similique, laudantium et totam repudiandae consequuntur nisi odit alias, earum
+                                dolorum, maxime mollitia veritatis quo!
+                            </p>
+                        </div>
+                        <div className={classes.confirmatory}>
+                            <h4>Confirmatory</h4>
+                            <p>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe rerum ea eum distinctio
+                                earum illo mollitia doloremque dolor temporibus molestiae.
+                            </p>
+                        </div>
+                    </div>
+                </>
+            ) : null}
+        </div>
+    )
 }
 export default DiagnosisPrev
