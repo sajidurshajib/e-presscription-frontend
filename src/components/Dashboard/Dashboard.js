0b14094274a5f8ecbbe9deda53d7ea244797eb50
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-09-18 16:25:49 +0600

    Context added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,17 +1,23 @@
+import { useReducer } from 'react'
+import { FieldContext } from '../../allContext'
+import { fieldState, fieldReducer } from '../../reducer/fieldReducer'
 import Body from '../Body/Body'
 import Nav from '../Nav/Nav'
 import classes from './Dashboard.module.css'
 import Sidebar from './Sidebar/Sidebar'
 
 const Dashboard = () => {
+    const [state, dispatch] = useReducer(fieldReducer, fieldState)
     return (
         <div className={`${classes.Dashboard}`}>
             {/* <div className={`${classes.Dashboard} ${classes.minimize}`}> */}
-            <Sidebar />
-            <div>
-                <Nav />
-                <Body />
-            </div>
+            <FieldContext.Provider value={{ state, dispatch }}>
+                <Sidebar />
+                <div>
+                    <Nav />
+                    <Body />
+                </div>
+            </FieldContext.Provider>
         </div>
     )
 }
