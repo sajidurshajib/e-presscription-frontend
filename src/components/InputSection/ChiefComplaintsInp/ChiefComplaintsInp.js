119bed68a9d36c379b501a766eedcb894df550e9
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-09-27 17:22:38 +0600

    chief complaints component  solved

--- src/components/InputSection/ChiefComplaintsInp/ChiefComplaintsInp.js
+++ src/components/InputSection/ChiefComplaintsInp/ChiefComplaintsInp.js
@@ -1,7 +1,17 @@
+import { useState } from 'react'
+import InputField from '../../ReUsable/InputField/InputField'
+import Suggestion from '../../ReUsable/Suggestion/Suggestion'
 import classes from './ChiefComplaintsInp.module.css'
 
 const ChiefComplaintsInp = () => {
-    return <div className={classes.ChiefComplaintsInp}></div>
+    const [text, setText] = useState('')
+    let arr = ['Fever for 7 days', 'Body Ache', 'Common cold']
+    return (
+        <div className={classes.ChiefComplaintsInp}>
+            <InputField text={text} setText={setText} label="Chief Complaints" />
+            {text.length !== 0 ? <Suggestion arr={arr} setText={setText} /> : null}
+        </div>
+    )
 }
 
 export default ChiefComplaintsInp
