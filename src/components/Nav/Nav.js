ed7588ff2b3b0d178851c5e09dbf17604a395de1
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-09-29 14:26:04 +0600

    sidebar nav key added

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,25 @@
-import { faPrescription, faSignInAlt, faSignOutAlt, faUserInjured, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import {
+    faAlignLeft,
+    faPrescription,
+    faSignInAlt,
+    faSignOutAlt,
+    faUserInjured,
+    faUserPlus,
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { SidebarContext } from '../../allContext'
 import classes from './Nav.module.css'
 
 const Nav = () => {
+    const { state, dispatch } = useContext(SidebarContext)
+    let h = state.expand ? 'hide' : 'unhide'
     return (
         <div className={classes.Nav}>
+            <span className={classes.alignLeft} onClick={() => dispatch({ type: h })}>
+                <FontAwesomeIcon icon={faAlignLeft} />
+            </span>
             <ul>
                 <li>
                     <Link to="/">
