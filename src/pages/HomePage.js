1ec8d98a9ba90c99a788b167f36303704a6ced1e
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-02 17:54:27 +0600

    HomePage added

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import Patients from '../components/Patients/Patients'
+
+const HomePage = () => {
+    return (
+        <div>
+            <Patients />
+        </div>
+    )
+}
+
+export default HomePage
