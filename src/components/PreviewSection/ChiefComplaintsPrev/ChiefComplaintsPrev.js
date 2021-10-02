f264378beb171a0fb15d578436bdf8c7c59c2956
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-02 17:55:46 +0600

    Modal add in chief preview component

--- src/components/PreviewSection/ChiefComplaintsPrev/ChiefComplaintsPrev.js
+++ src/components/PreviewSection/ChiefComplaintsPrev/ChiefComplaintsPrev.js
@@ -1,20 +1,28 @@
 import { useState } from 'react'
+import EditAndDelete from '../EditAndDelete/EditAndDelete'
 import classes from './ChiefComplaintsPrev.module.css'
 
 const ChiefComplaintsPrev = () => {
     const [toggle, setToggle] = useState(true)
+    const [editValue, setEditValue] = useState(null)
+
+    const demo = ['Fever for 7 days', 'Body Ache', 'Common Cold', 'Sore Throat']
 
     return (
         <div className={classes.ChiefComplaintsPrev}>
             <h3 onClick={() => setToggle(!toggle)} className={toggle ? classes.toggle : null}>
                 Chief Complaints
             </h3>
+            {editValue !== null ? <EditAndDelete editValue={setEditValue} value={editValue} /> : null}
             {toggle ? (
                 <ul>
-                    <li>Fever for 7 days</li>
-                    <li>Body Ache</li>
-                    <li>Common Cold</li>
-                    <li>Sore Throat</li>
+                    {demo.map((v, i) => {
+                        return (
+                            <li key={i}>
+                                {v} <span onClick={() => setEditValue({ v, i })}>Edit</span>
+                            </li>
+                        )
+                    })}
                 </ul>
             ) : null}
         </div>
