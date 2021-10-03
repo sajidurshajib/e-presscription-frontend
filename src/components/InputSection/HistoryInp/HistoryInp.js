8f2f72940e60ff60f2e35c9ed0df61bd791db3ce
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-03 16:36:30 +0600

    history input field added

--- src/components/InputSection/HistoryInp/HistoryInp.js
+++ src/components/InputSection/HistoryInp/HistoryInp.js
@@ -1,7 +1,21 @@
+import { useState } from 'react'
+import InputField from '../../ReUsable/InputField/InputField'
+import Suggestion from '../../ReUsable/Suggestion/Suggestion'
+import classes from './HistoryInp.module.css'
+
 const HistoryInp = () => {
+    const [personal, setPersonal] = useState('')
+    const [professional, setProfessional] = useState('')
+
+    let arr = ['Fever for 7 days', 'Body Ache', 'Common cold']
+
     return (
-        <div>
-            <h3>History</h3>
+        <div className={classes.HistoryInp}>
+            <InputField text={personal} setText={setPersonal} label="Personal History" />
+            {personal.length !== 0 ? <Suggestion arr={arr} setText={setPersonal} /> : null}
+
+            <InputField text={professional} setText={setProfessional} label="Professional History" />
+            {professional.length !== 0 ? <Suggestion arr={arr} setText={setProfessional} /> : null}
         </div>
     )
 }
