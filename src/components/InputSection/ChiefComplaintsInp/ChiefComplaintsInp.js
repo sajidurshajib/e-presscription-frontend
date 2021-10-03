f9cf98109caa1e36cd8bd6cfd199f7d87f6077aa
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-03 16:38:14 +0600

    Not solved input search

--- src/components/InputSection/ChiefComplaintsInp/ChiefComplaintsInp.js
+++ src/components/InputSection/ChiefComplaintsInp/ChiefComplaintsInp.js
@@ -6,10 +6,19 @@ import classes from './ChiefComplaintsInp.module.css'
 const ChiefComplaintsInp = () => {
     const [text, setText] = useState('')
     let arr = ['Fever for 7 days', 'Body Ache', 'Common cold']
+
     return (
         <div className={classes.ChiefComplaintsInp}>
             <InputField text={text} setText={setText} label="Chief Complaints" />
-            {text.length !== 0 ? <Suggestion arr={arr} setText={setText} /> : null}
+
+            {text ? (
+                <Suggestion
+                    arr={arr.filter((val) => val.toLocaleLowerCase().includes(text.toLowerCase()))}
+                    setText={setText}
+                />
+            ) : null}
+
+            {/* {text.length !== 0 ? <Suggestion arr={arr} setText={setText} /> : null} */}
         </div>
     )
 }
