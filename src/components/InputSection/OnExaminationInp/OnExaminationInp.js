3eb671b5428061894a24764d34d93096ccce255f
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-04 17:48:38 +0600

    OnExamination component filled

--- src/components/InputSection/OnExaminationInp/OnExaminationInp.js
+++ src/components/InputSection/OnExaminationInp/OnExaminationInp.js
@@ -1,7 +1,41 @@
+import { Fragment, useState } from 'react'
+import AddField from './AddField'
+import classes from './OnExaminationInp.module.css'
+
 const OnExaminationInp = () => {
+    let arr = [
+        { id: 1, name: 'BP', value: '' },
+        { id: 2, name: 'Pulse', value: '' },
+        { id: 3, name: 'Temp', value: '' },
+    ]
+
+    const [onExam, setOnExam] = useState(arr)
+    const [add, setAdd] = useState('')
+
+    const addData = (e) => {
+        e.preventDefault()
+
+        let a = onExam
+        let b = { id: a[a.length - 1].id + 1, name: add, value: '' }
+        a.push(b)
+
+        setOnExam([...a])
+        setAdd('')
+    }
+
     return (
-        <div>
-            <h3>On-Examination</h3>
+        <div className={classes.OnExaminationInp}>
+            {onExam.map((v) => {
+                return (
+                    <Fragment key={v.id}>
+                        <div className={classes.Inp}>
+                            <input type="text" required />
+                            <label>{v.name}</label>
+                        </div>
+                    </Fragment>
+                )
+            })}
+            <AddField addData={addData} add={add} setAdd={setAdd} />
         </div>
     )
 }
