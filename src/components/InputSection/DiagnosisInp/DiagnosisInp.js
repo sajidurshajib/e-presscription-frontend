99b8355b0883795819ad5f0ec5db4724f104c401
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-05 16:21:10 +0600

    Dignosis designed

--- src/components/InputSection/DiagnosisInp/DiagnosisInp.js
+++ src/components/InputSection/DiagnosisInp/DiagnosisInp.js
@@ -1,7 +1,15 @@
+import { useState } from 'react'
+import TextField from '../../ReUsable/TextField/TextField'
+import classes from './DiagnosisInp.module.css'
+
 const DiagnosisInp = () => {
+    const [probable, setProbable] = useState('')
+    const [confirm, setConfirm] = useState('')
+
     return (
-        <div>
-            <h3>Diagnosis</h3>
+        <div className={classes.DiagnosisInp}>
+            <TextField label={'Probable Dx'} text={probable} setText={setProbable} />
+            <TextField label={'Confirmatory Dx'} text={confirm} setTex={setConfirm} />
         </div>
     )
 }
