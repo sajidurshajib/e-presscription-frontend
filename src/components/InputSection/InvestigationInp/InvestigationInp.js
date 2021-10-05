374aaf57d90464667db2a91f4613af4fc41ad472
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-05 16:25:12 +0600

    Investigation half designed

--- src/components/InputSection/InvestigationInp/InvestigationInp.js
+++ src/components/InputSection/InvestigationInp/InvestigationInp.js
@@ -1,7 +1,22 @@
+import { useState } from 'react'
+import InputField from '../../ReUsable/InputField/InputField'
+import Suggestion from '../../ReUsable/Suggestion/Suggestion'
+import classes from './InvestigationInp.module.css'
+
 const InvestigationInp = () => {
+    const [text, setText] = useState('')
+    let arr = ['Fever for 7 days', 'Body Ache', 'Common cold']
+
     return (
-        <div>
-            <h3>Investigation</h3>
+        <div className={classes.InvestigationInp}>
+            <InputField text={text} setText={setText} label="Investigation" />
+
+            {text ? (
+                <Suggestion
+                    arr={arr.filter((val) => val.toLocaleLowerCase().includes(text.toLowerCase()))}
+                    setText={setText}
+                />
+            ) : null}
         </div>
     )
 }
