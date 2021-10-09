feabad3fd85ae51f91c9b6a6bde0e48368daf48b
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-09 13:04:48 +0600

    Suggestion length check

--- src/components/ReUsable/Suggestion/Suggestion.js
+++ src/components/ReUsable/Suggestion/Suggestion.js
@@ -3,15 +3,17 @@ import classes from './Suggestion.module.css'
 const Suggestion = ({ arr, setText }) => {
     return (
         <div className={classes.Suggestion}>
-            <ul>
-                {arr.map((v, i) => {
-                    return (
-                        <li key={i} onClick={() => setText(v)}>
-                            {v}
-                        </li>
-                    )
-                })}
-            </ul>
+            {arr.length !== 0 ? (
+                <ul>
+                    {arr.map((v, i) => {
+                        return (
+                            <li key={i} onClick={() => setText(v)}>
+                                {v}
+                            </li>
+                        )
+                    })}
+                </ul>
+            ) : null}
         </div>
     )
 }
