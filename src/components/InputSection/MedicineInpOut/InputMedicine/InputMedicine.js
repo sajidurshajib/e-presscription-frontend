636bb8defe66fe09a500c1a82cc7809d4c2a5ac2
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-10 17:25:37 +0600

    input medicine designed

--- src/components/InputSection/MedicineInpOut/InputMedicine/InputMedicine.js
+++ src/components/InputSection/MedicineInpOut/InputMedicine/InputMedicine.js
@@ -1,8 +1,15 @@
+import DropDown from '../../../ReUsable/DropDown/DropDown'
 import InputField from '../../../ReUsable/InputField/InputField'
 import InputNumber from '../../../ReUsable/InputNumber/InputNumber'
+// import TextField from '../../../ReUsable/TextField/TextField'
 import classes from './InputMedicine.module.css'
 
 const InputMedicine = ({ setMedicine }) => {
+    const option = [
+        { name: 'After Meal', value: 'after' },
+        { name: 'Before Meal', value: 'before' },
+    ]
+
     return (
         <div className={classes.InputMedicine}>
             <form>
@@ -12,14 +19,13 @@ const InputMedicine = ({ setMedicine }) => {
                         <InputNumber label="Morning" />
                         <InputNumber label="Evening" />
                         <InputNumber label="Night" />
-                        <select name="beforeAfter" id="">
-                            <option value="after">After meal</option>
-                            <option value="before">Before meal</option>
-                        </select>
+                        <DropDown arr={option} />
                         <InputNumber label="Day" />
                     </div>
                 </div>
+                {/* <TextField /> */}
                 <InputField label="Remark" />
+                <button>Submit</button>
             </form>
         </div>
     )
