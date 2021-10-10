be43b6abea40d76d298ec843fabfed5cf71ddd34
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-10 17:31:59 +0600

    input type number reusable

--- src/components/ReUsable/InputNumber/InputNumber.js
+++ src/components/ReUsable/InputNumber/InputNumber.js
@@ -4,7 +4,7 @@ const InputNumber = ({ label }) => {
     return (
         <div className={classes.InputNumber}>
             <form>
-                <input type="number" />
+                <input type="number" required />
                 <label>{label}</label>
             </form>
         </div>
