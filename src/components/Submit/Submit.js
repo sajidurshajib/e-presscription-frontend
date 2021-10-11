fa371b4b345a9af6c41d7b21e44257424108c9c3
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-11 10:41:11 +0600

    Submit component created

---
+++ src/components/Submit/Submit.js
@@ -0,0 +1,12 @@
+import classes from './Submit.module.css'
+
+const Submit = () => {
+    return (
+        <div className={classes.Submit}>
+            <button>Template</button>
+            <button>Submit</button>
+        </div>
+    )
+}
+
+export default Submit
