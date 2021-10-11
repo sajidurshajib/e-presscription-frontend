a227b16ae7730161ee9067d7aab3d1c7c092f655
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-11 15:40:07 +0600

    Advice and Nextfollowup component added

--- src/components/InputSection/InputSection.js
+++ src/components/InputSection/InputSection.js
@@ -1,11 +1,13 @@
 import { useContext } from 'react'
 import { FieldContext } from '../../allContext'
 import Submit from '../Submit/Submit'
+import AdviceInpOut from './AdviceInpOut/AdviceInpOut'
 import ChiefComplaintsInp from './ChiefComplaintsInp/ChiefComplaintsInp'
 import DiagnosisInp from './DiagnosisInp/DiagnosisInp'
 import HistoryInp from './HistoryInp/HistoryInp'
 import InvestigationInp from './InvestigationInp/InvestigationInp'
 import MedicineInpOut from './MedicineInpOut/MedicineInpOut'
+import NextFollowUpInpOut from './NextFollowUpInpOut/NextFollowUpInpOut'
 import OnExaminationInp from './OnExaminationInp/OnExaminationInp'
 
 const InputSection = () => {
@@ -18,6 +20,8 @@ const InputSection = () => {
             {state.field === 'investigation' ? <InvestigationInp /> : null}
             {state.field === 'onexam' ? <OnExaminationInp /> : null}
             {state.field === 'medicine' ? <MedicineInpOut /> : null}
+            {state.field === 'advice' ? <AdviceInpOut /> : null}
+            {state.field === 'next' ? <NextFollowUpInpOut /> : null}
             <Submit />
         </div>
     )
