7f7822cef976a8fdec6cb673b787ce6033d7c142
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-12 17:22:20 +0600

    width fix

--- src/components/InputSection/ChiefComplaintsInp/ChiefComplaintsInp.js
+++ src/components/InputSection/ChiefComplaintsInp/ChiefComplaintsInp.js
@@ -1,24 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { lastLine } from '../../../utils/Lines'
 import Suggestion from '../../ReUsable/Suggestion/Suggestion'
 import TextField from '../../ReUsable/TextField/TextField'
 import classes from './ChiefComplaintsInp.module.css'
 
 const ChiefComplaintsInp = () => {
     const [text, setText] = useState('')
-    let arr = ['Fever for 7 days', 'Body Ache', 'Common cold']
+    const [cc, setCc] = useState('')
 
+    const concatSet = (conc) => {
+        let lastIndex = text.lastIndexOf('\n')
+        let str = text.substring(0, lastIndex + 1)
+        let a = str + conc
+        setText(a)
+    }
+
+    // Fetch
+    useEffect(() => {
+        const funFetch = async () => {
+            try {
+                const response = await fetch(`/ccs?search=${lastLine(text)}&page_size=10`)
+                if (response.ok) {
+                    const data = await response.json()
+                    const formatedData = data.map(({ cc, id }) => ({ name: cc, id }))
+                    setCc(formatedData)
+                }
+            } catch (err) {}
+        }
+        if (lastLine(text)) {
+            funFetch()
+        }
+    }, [text, setCc])
+
+    // Return
     return (
         <div className={classes.ChiefComplaintsInp}>
-            {/* <InputField text={text} setText={setText} label="Chief Complaints" /> */}
-
             <TextField text={text} setText={setText} label="Chief Complaints" />
 
-            {text ? (
-                <Suggestion
-                    arr={arr.filter((val) => val.toLocaleLowerCase().includes(text.toLowerCase()))}
-                    setText={setText}
-                />
-            ) : null}
+            {lastLine(text) ? <Suggestion arr={cc} setText={concatSet} /> : null}
         </div>
     )
 }
