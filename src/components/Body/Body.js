d1cc13a5a6cc0550a4fee3da0ae2304090289169
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-16 15:25:36 +0600

    chiefComplaints context wrap

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,16 +1,24 @@
+import { useReducer } from 'react'
+import { ChiefComplaints } from '../../allContext'
+import { chiefState, chiefReducer } from '../../reducer/chiefReducer'
 import InputSection from '../InputSection/InputSection'
 import PreviewSection from '../PreviewSection/PreviewSection'
 import classes from './Body.module.css'
 
 const Body = () => {
+    const [stateChief, dispatchChief] = useReducer(chiefReducer, chiefState)
     return (
         <div className={classes.Body}>
-            <div>
-                <PreviewSection />
-            </div>
-            <div>
-                <InputSection />
-            </div>
+            <ChiefComplaints.Provider value={{ stateChief, dispatchChief }}>
+                {/* {Context Wrapper} */}
+                <div>
+                    <PreviewSection />
+                </div>
+                <div>
+                    <InputSection />
+                </div>
+                {/* {Context Wrapper} */}
+            </ChiefComplaints.Provider>
         </div>
     )
 }
