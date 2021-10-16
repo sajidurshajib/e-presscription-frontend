55e594615f827a435ae0beee81469e35214553d3
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-16 17:36:23 +0600

    Investigation input done

--- src/components/InputSection/InvestigationInp/InvestigationInp.js
+++ src/components/InputSection/InvestigationInp/InvestigationInp.js
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
+import { useContext } from 'react'
+import { Investigation } from '../../../allContext'
 import InputField from '../../ReUsable/InputField/InputField'
 import Suggestion from '../../ReUsable/Suggestion/Suggestion'
 import classes from './InvestigationInp.module.css'
 
 const InvestigationInp = () => {
+    const { stateInvestigation, dispatchInvestigation } = useContext(Investigation)
+
     const [text, setText] = useState('')
     const [tests, setTests] = useState('')
 
@@ -22,12 +26,21 @@ const InvestigationInp = () => {
         }
     }, [text, setTests])
 
+    const submit = (e) => {
+        e.preventDefault()
+        dispatchInvestigation({
+            type: 'input',
+            payload: stateInvestigation.inv.length === 0 ? [text] : [...stateInvestigation.inv, text],
+        })
+        setText('')
+    }
+
     return (
         <div className={classes.InvestigationInp}>
             <div className={classes.wrap}>
                 <InputField text={text} setText={setText} label="Investigation" />
                 {text ? <Suggestion arr={tests} setText={setText} /> : null}
-                <button type="submit">Submit</button>
+                <button onClick={submit}>Submit</button>
             </div>
         </div>
     )
