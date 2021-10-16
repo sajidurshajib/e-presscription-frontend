fa194cfb942eb29e25b9848004edfc93ccefdc34
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-16 11:04:06 +0600

    Data context and reducer removed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import { useReducer } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { DataContext } from './allContext'
 import EprescriptionPage from './pages/EprescriptionPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
-import { dataState, dataReducer } from './reducer/dataReducer'
 
 const App = () => {
-    const [stateData, dispatchData] = useReducer(dataReducer, dataState)
     return (
         <div>
             {/* Router */}
             <Router>
                 <Switch>
                     <Route exact={true} path="/">
-                        {/* Data Context */}
-                        <DataContext.Provider value={{ state: stateData, dispatch: dispatchData }}>
-                            <EprescriptionPage />
-                        </DataContext.Provider>
+                        <EprescriptionPage />
                     </Route>
                     <Route path="/login">
                         <LoginPage />
--- src/allContext.js
+++ src/allContext.js
@@ -1,5 +1,4 @@
 import { createContext } from 'react'
 
-export const DataContext = createContext()
 export const FieldContext = createContext()
 export const SidebarContext = createContext()
