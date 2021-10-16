4bc35526ba119d749a0f2124f606249d6a959af0
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-16 17:34:27 +0600

    Investigation Context wrap

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,23 +1,27 @@
 import { useReducer } from 'react'
-import { ChiefComplaints } from '../../allContext'
+import { ChiefComplaints, Investigation } from '../../allContext'
 import { chiefState, chiefReducer } from '../../reducer/chiefReducer'
+import { investigationReducer, investigationState } from '../../reducer/investigationReducer'
 import InputSection from '../InputSection/InputSection'
 import PreviewSection from '../PreviewSection/PreviewSection'
 import classes from './Body.module.css'
 
 const Body = () => {
     const [stateChief, dispatchChief] = useReducer(chiefReducer, chiefState)
+    const [stateInvestigation, dispatchInvestigation] = useReducer(investigationReducer, investigationState)
     return (
         <div className={classes.Body}>
             <ChiefComplaints.Provider value={{ stateChief, dispatchChief }}>
-                {/* {Context Wrapper} */}
-                <div>
-                    <PreviewSection />
-                </div>
-                <div>
-                    <InputSection />
-                </div>
-                {/* {Context Wrapper} */}
+                <Investigation.Provider value={{ stateInvestigation, dispatchInvestigation }}>
+                    {/* {Context Wrapper} */}
+                    <div>
+                        <PreviewSection />
+                    </div>
+                    <div>
+                        <InputSection />
+                    </div>
+                    {/* {Context Wrapper} */}
+                </Investigation.Provider>
             </ChiefComplaints.Provider>
         </div>
     )
