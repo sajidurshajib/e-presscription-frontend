97599491dfbb1850dfecd0dc37cfca5bb19ef433
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-17 12:42:02 +0600

    Diagnosis Context created

--- src/allContext.js
+++ src/allContext.js
@@ -5,3 +5,4 @@ export const SidebarContext = createContext()
 
 export const ChiefComplaints = createContext()
 export const Investigation = createContext()
+export const Diagnosis = createContext()
