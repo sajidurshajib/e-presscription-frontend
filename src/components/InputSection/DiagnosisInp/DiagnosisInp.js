1a799bf619106856d4614ab939f9c88b23040cbc
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-17 12:47:30 +0600

    Diagnosis input in context and reducer

--- src/components/InputSection/DiagnosisInp/DiagnosisInp.js
+++ src/components/InputSection/DiagnosisInp/DiagnosisInp.js
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { Diagnosis } from '../../../allContext'
 import TextField from '../../ReUsable/TextField/TextField'
 import classes from './DiagnosisInp.module.css'
 
 const DiagnosisInp = () => {
+    const { dispatchDiagnosis } = useContext(Diagnosis)
+
     const [probable, setProbable] = useState('')
     const [confirm, setConfirm] = useState('')
 
+    const probableSubmit = (e) => {
+        e.preventDefault()
+        dispatchDiagnosis({ type: 'prbl', payload: probable })
+    }
+
+    const confirmSubmit = (e) => {
+        e.preventDefault()
+        dispatchDiagnosis({ type: 'cnf', payload: confirm })
+    }
     return (
         <div className={classes.DiagnosisInp}>
             <TextField label={'Probable Dx'} text={probable} setText={setProbable} />
-            <TextField label={'Confirmatory Dx'} text={confirm} setTex={setConfirm} />
+            <button onClick={probableSubmit}>Submit</button>
+            <TextField label={'Confirmatory Dx'} text={confirm} setText={setConfirm} />
+            <button onClick={confirmSubmit}>Submit</button>
         </div>
     )
 }
