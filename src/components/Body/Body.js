fde3f434f10f5a605e07b33fe1bd0de08ef71501
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-17 12:46:18 +0600

    Diagnosis context wrapper

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,6 +1,7 @@
 import { useReducer } from 'react'
-import { ChiefComplaints, Investigation } from '../../allContext'
+import { ChiefComplaints, Investigation, Diagnosis } from '../../allContext'
 import { chiefState, chiefReducer } from '../../reducer/chiefReducer'
+import { diagnosisReducer, diagnosisState } from '../../reducer/diagnosisReducer'
 import { investigationReducer, investigationState } from '../../reducer/investigationReducer'
 import InputSection from '../InputSection/InputSection'
 import PreviewSection from '../PreviewSection/PreviewSection'
@@ -9,18 +10,21 @@ import classes from './Body.module.css'
 const Body = () => {
     const [stateChief, dispatchChief] = useReducer(chiefReducer, chiefState)
     const [stateInvestigation, dispatchInvestigation] = useReducer(investigationReducer, investigationState)
+    const [stateDiagnosis, dispatchDiagnosis] = useReducer(diagnosisReducer, diagnosisState)
     return (
         <div className={classes.Body}>
             <ChiefComplaints.Provider value={{ stateChief, dispatchChief }}>
                 <Investigation.Provider value={{ stateInvestigation, dispatchInvestigation }}>
-                    {/* {Context Wrapper} */}
-                    <div>
-                        <PreviewSection />
-                    </div>
-                    <div>
-                        <InputSection />
-                    </div>
-                    {/* {Context Wrapper} */}
+                    <Diagnosis.Provider value={{ stateDiagnosis, dispatchDiagnosis }}>
+                        {/* {Context Wrapper} */}
+                        <div>
+                            <PreviewSection />
+                        </div>
+                        <div>
+                            <InputSection />
+                        </div>
+                        {/* {Context Wrapper} */}
+                    </Diagnosis.Provider>
                 </Investigation.Provider>
             </ChiefComplaints.Provider>
         </div>
