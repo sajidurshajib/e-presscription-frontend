d81fe9ae9ad0f84ce1dabbe5ab03036a6e7e80f2
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-19 11:03:51 +0600

    TextField onSubmit removed

--- src/components/ReUsable/TextField/TextField.js
+++ src/components/ReUsable/TextField/TextField.js
@@ -1,17 +1,12 @@
 import classes from './TextField.module.css'
 
 const TextField = ({ label, text, setText }) => {
-    const onSubmit = (e) => {
-        e.preventDefault()
-        setText('')
-    }
-
     let lineBreak = text.split('\n')
     let count = lineBreak.length
 
     return (
         <div className={classes.TextField}>
-            <form onSubmit={(e) => onSubmit(e)}>
+            <form>
                 <label htmlFor="">{label}</label>
                 <textarea cols="50" rows={count + 1} value={text} onChange={(e) => setText(e.target.value)} required />
                 <span></span>
