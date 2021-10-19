4f06ebf9bb94e4cc17971cacded689be22ced7e3
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-19 16:22:05 +0600

    onSubmit bug removed

--- src/components/ReUsable/InputNumber/InputNumber.js
+++ src/components/ReUsable/InputNumber/InputNumber.js
@@ -3,10 +3,10 @@ import classes from './InputNumber.module.css'
 const InputNumber = ({ label, num, setNum }) => {
     return (
         <div className={classes.InputNumber}>
-            <form>
+            <div className={classes.wrap}>
                 <input type="number" value={num} onChange={(e) => setNum(parseInt(e.target.value))} required />
                 <label>{label}</label>
-            </form>
+            </div>
         </div>
     )
 }
