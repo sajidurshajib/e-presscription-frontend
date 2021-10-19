ef705e08b85bcb396b58926ef4cd228a6d640c33
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-19 16:26:24 +0600

    onSubmit bug removed

--- src/components/ReUsable/InputField/InputField.js
+++ src/components/ReUsable/InputField/InputField.js
@@ -1,18 +1,12 @@
 import classes from './InputField.module.css'
 
 const InputField = ({ text, setText, label }) => {
-    const onSubmit = (e) => {
-        e.preventDefault()
-    }
-
     return (
         <div className={classes.InputField}>
-            <form onSubmit={(e) => onSubmit(e)}>
-                <input type="text" onChange={(e) => setText(e.target.value)} value={text} required />
-                <label>
-                    <span>{label}</span>
-                </label>
-            </form>
+            <input type="text" onChange={(e) => setText(e.target.value)} value={text} required />
+            <label>
+                <span>{label}</span>
+            </label>
         </div>
     )
 }
