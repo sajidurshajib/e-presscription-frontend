85d6e8d0969a293d08dc2ccd81a9a9263d235756
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-21 16:52:58 +0600

    Remark toggle design input

--- src/components/InputSection/HistoryInp/CoMorbidity/CoMorbidity.js
+++ src/components/InputSection/HistoryInp/CoMorbidity/CoMorbidity.js
@@ -2,7 +2,11 @@ import { useState } from 'react'
 import classes from './CoMorbidity.module.css'
 
 const CoMorbidity = () => {
-    const [htn, setHtn] = useState('')
+    const [htn, setHtn] = useState(false)
+    const [dm, setDm] = useState(false)
+    const [tb, setTb] = useState(false)
+    const [asm, setAsm] = useState(false)
+
     return (
         <div className={classes.CoMorbidity}>
             <h3>
@@ -10,24 +14,32 @@ const CoMorbidity = () => {
             </h3>
             <div className={classes.wrapper}>
                 <div className={classes.check}>
-                    <p>Hypertension (HTN)</p>
-                    <button>+/-</button>
-                    <input type="text" />
+                    <p>
+                        Hypertension (HTN)<span>{htn ? '+' : null}</span>
+                    </p>
+                    <button onClick={() => setHtn(!htn)}>+/-</button>
+                    {htn ? <input type="text" /> : null}
                 </div>
                 <div className={classes.check}>
-                    <p>Diabetes (DM)</p>
-                    <button>+/-</button>
-                    <input type="text" />
+                    <p>
+                        Diabetes (DM)<span>{dm ? '+' : null}</span>
+                    </p>
+                    <button onClick={() => setDm(!dm)}>+/-</button>
+                    {dm ? <input type="text" /> : null}
                 </div>
                 <div className={classes.check}>
-                    <p>Tuberculosis (TB)</p>
-                    <button>+/-</button>
-                    <input type="text" />
+                    <p>
+                        Tuberculosis (TB)<span>{tb ? '+' : null}</span>
+                    </p>
+                    <button onClick={() => setTb(!tb)}>+/-</button>
+                    {tb ? <input type="text" /> : null}
                 </div>
                 <div className={classes.check}>
-                    <p>Asthma</p>
-                    <button>+/-</button>
-                    <input type="text" />
+                    <p>
+                        Asthma<span>{asm ? '+' : null}</span>
+                    </p>
+                    <button onClick={() => setAsm(!asm)}>+/-</button>
+                    {asm ? <input type="text" /> : null}
                 </div>
             </div>
         </div>
