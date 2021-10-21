5a3b35602e79b6ad4d9615424a567095689b41c6
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-21 16:57:42 +0600

    Array removed

--- src/components/InputSection/OnExaminationInp/OnExaminationInp.js
+++ src/components/InputSection/OnExaminationInp/OnExaminationInp.js
@@ -1,41 +1,32 @@
+import { Fragment, useState } from 'react'
 import ExamField from './ExamField/ExamField'
 import classes from './OnExaminationInp.module.css'
 
 const OnExaminationInp = () => {
-    // let arr = []
-    // let sgg = [
-    //     'Anaemia',
-    //     'Jaundice',
-    //     'Cyanosis',
-    //     'Blood Pressure',
-    //     'Pulse',
-    //     'Heart',
-    //     'Lungs',
-    //     'Heart rate',
-    //     'Respiratory Rate',
-    //     'Oedema',
-    //     'Hair Distribution',
-    //     'Height',
-    //     'Weight',
-    //     'Temperature',
-    // ]
-
+    const [more, setMore] = useState(false)
     return (
         <div className={classes.OnExaminationInp}>
-            <ExamField name="anaemia" />
-            <ExamField name="jaundice" />
-            <ExamField name="cyanosis" />
             <ExamField name="bloodpressure" />
             <ExamField name="pulse" />
-            <ExamField name="heart" />
-            <ExamField name="lungs" />
-            <ExamField name="heartrate" />
-            <ExamField name="respiratory" />
-            <ExamField name="odema" />
-            <ExamField name="weight" />
-            <ExamField name="height" />
             <ExamField name="temp" />
             <ExamField name="rbs" />
+            <ExamField name="heart" />
+            <ExamField name="lungs" />
+            <ExamField name="weight" />
+
+            {more ? (
+                <Fragment>
+                    <ExamField name="anaemia" />
+                    <ExamField name="jaundice" />
+                    <ExamField name="cyanosis" />
+                    <ExamField name="heartrate" />
+                    <ExamField name="respiratory" />
+                    <ExamField name="oedema" />
+                    <ExamField name="height" />
+                </Fragment>
+            ) : null}
+
+            <button onClick={() => setMore(!more)}>See {more ? 'less' : 'more'}</button>
         </div>
     )
 }
