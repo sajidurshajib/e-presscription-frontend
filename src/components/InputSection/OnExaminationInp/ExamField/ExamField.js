ece36dc3534058a6ef5280046e50f1b37c880f45
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-21 16:56:52 +0600

    Serial changed

--- src/components/InputSection/OnExaminationInp/ExamField/ExamField.js
+++ src/components/InputSection/OnExaminationInp/ExamField/ExamField.js
@@ -4,6 +4,86 @@ import classes from './ExamField.module.css'
 const ExamField = ({ name }) => {
     return (
         <div className={classes.ExamField}>
+            {/* Blood Pressure*/}
+            {name === 'bloodpressure' ? (
+                <Fragment>
+                    <div className={classes.wrap4}>
+                        <p>Blood Pressure :</p>
+                        <input type="number" placeholder="Systolic" required />
+                        <span>/</span>
+                        <input type="number" placeholder="Diastolic" required />
+                        <p>mm of Hg</p>
+                    </div>
+                </Fragment>
+            ) : null}
+
+            {/* Pluse*/}
+            {name === 'pulse' ? (
+                <Fragment>
+                    <div className={classes.wrap2}>
+                        <p>Pulse :</p>
+                        <input type="number" name="" id="" required />
+                        <p>beats/min</p>
+                    </div>
+                </Fragment>
+            ) : null}
+
+            {/* Temp*/}
+            {name === 'temp' ? (
+                <Fragment>
+                    <div className={classes.wrap3}>
+                        <p>Temp : </p>
+                        <input type="number" required />
+                        <select>
+                            <option value="f"> &deg; F </option>
+                            <option value="c"> &deg; C </option>
+                        </select>
+                    </div>
+                </Fragment>
+            ) : null}
+
+            {/* RBS*/}
+            {name === 'rbs' ? (
+                <Fragment>
+                    <div className={classes.wrap2}>
+                        <p>RBS : </p>
+                        <input type="number" required />
+                        <p>mmol/L</p>
+                    </div>
+                </Fragment>
+            ) : null}
+
+            {/* Heart*/}
+            {name === 'heart' ? (
+                <Fragment>
+                    <div className={classes.wrap}>
+                        <p>Heart :</p>
+                        <input type="text" required />
+                    </div>
+                </Fragment>
+            ) : null}
+
+            {/* Lungs*/}
+            {name === 'lungs' ? (
+                <Fragment>
+                    <div className={classes.wrap}>
+                        <p>Lungs :</p>
+                        <input type="text" required />
+                    </div>
+                </Fragment>
+            ) : null}
+
+            {/* Weight*/}
+            {name === 'weight' ? (
+                <Fragment>
+                    <div className={classes.wrap2}>
+                        <p>Weight : </p>
+                        <input type="number" required />
+                        <p>Kg</p>
+                    </div>
+                </Fragment>
+            ) : null}
+
             {/* Anaemia */}
             {name === 'anaemia' ? (
                 <Fragment>
@@ -46,49 +126,6 @@ const ExamField = ({ name }) => {
                 </Fragment>
             ) : null}
 
-            {/* Blood Pressure*/}
-            {name === 'bloodpressure' ? (
-                <Fragment>
-                    <div className={classes.wrap4}>
-                        <p>Blood Pressure :</p>
-                        <input type="number" placeholder="Systolic" required />/
-                        <input type="number" placeholder="Diastolic" required />
-                        <p>mm of Hg</p>
-                    </div>
-                </Fragment>
-            ) : null}
-
-            {/* Pluse*/}
-            {name === 'pulse' ? (
-                <Fragment>
-                    <div className={classes.wrap2}>
-                        <p>Pulse :</p>
-                        <input type="number" name="" id="" required />
-                        <p>beats/min</p>
-                    </div>
-                </Fragment>
-            ) : null}
-
-            {/* Heart*/}
-            {name === 'heart' ? (
-                <Fragment>
-                    <div className={classes.wrap}>
-                        <p>Heart :</p>
-                        <input type="text" required />
-                    </div>
-                </Fragment>
-            ) : null}
-
-            {/* Lungs*/}
-            {name === 'lungs' ? (
-                <Fragment>
-                    <div className={classes.wrap}>
-                        <p>Lungs :</p>
-                        <input type="text" required />
-                    </div>
-                </Fragment>
-            ) : null}
-
             {/* Heart rate*/}
             {name === 'heartrate' ? (
                 <Fragment>
@@ -111,27 +148,16 @@ const ExamField = ({ name }) => {
                 </Fragment>
             ) : null}
 
-            {/* Odema*/}
-            {name === 'odema' ? (
+            {/* Oedema*/}
+            {name === 'oedema' ? (
                 <Fragment>
                     <div className={classes.wrap1}>
-                        <p>Odema : </p>
+                        <p>Oedema : </p>
                         <select>
                             <option value="-">-</option>
                             <option value="+">+</option>
                         </select>
                         <input type="text" placeholder="Remark" required />
-                    </div>
-                </Fragment>
-            ) : null}
-
-            {/* Weight*/}
-            {name === 'weight' ? (
-                <Fragment>
-                    <div className={classes.wrap2}>
-                        <p>Weight : </p>
-                        <input type="number" required />
-                        <p>Kg</p>
                     </div>
                 </Fragment>
             ) : null}
@@ -145,31 +171,6 @@ const ExamField = ({ name }) => {
                         <p>Feet</p>
                         <input type="number" required />
                         <p>Inch</p>
-                    </div>
-                </Fragment>
-            ) : null}
-
-            {/* Temp*/}
-            {name === 'temp' ? (
-                <Fragment>
-                    <div className={classes.wrap3}>
-                        <p>Temp : </p>
-                        <input type="number" required />
-                        <select>
-                            <option value="f"> &deg; F </option>
-                            <option value="c"> &deg; C </option>
-                        </select>
-                    </div>
-                </Fragment>
-            ) : null}
-
-            {/* RBS*/}
-            {name === 'rbs' ? (
-                <Fragment>
-                    <div className={classes.wrap2}>
-                        <p>RBS : </p>
-                        <input type="number" required />
-                        <p>mmol/L</p>
                     </div>
                 </Fragment>
             ) : null}
