699ae69ee8bb2bae32518bd1bb604c541ccac439
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-27 10:58:39 +0600

    History context added

--- src/allContext.js
+++ src/allContext.js
@@ -4,6 +4,7 @@ export const FieldContext = createContext()
 export const SidebarContext = createContext()
 
 export const ChiefComplaints = createContext()
+export const History = createContext()
 export const Investigation = createContext()
 export const Diagnosis = createContext()
 export const Advice = createContext()
