96e566c02b7ca615a0d99c95584ddc31567cb663
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-27 17:32:42 +0600

    coMorbidity reducer added

---
+++ src/reducer/coMorbidityReducer.js
@@ -0,0 +1,17 @@
+import { EpGetStorage, EpSetStorage, EpDefStorage } from '../utils/EpLocalStorage'
+
+//Set state as default storage value
+export const coMorbidityState = EpDefStorage('coMorbidity', { coMorbidity: [] })
+
+export const coMorbidityReducer = (state, action) => {
+    switch (action.type) {
+        case 'input':
+            EpSetStorage('coMorbidity', { coMorbidity: action.payload })
+            return JSON.parse(EpGetStorage('coMorbidity'))
+        case 'remove':
+            EpDefStorage('coMorbidity', { coMorbidity: '' })
+            return JSON.parse(EpGetStorage('coMorbidity'))
+        default:
+            return state
+    }
+}
