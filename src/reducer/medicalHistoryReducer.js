ee1fd5fcedc1de60f011d44029984c14af3ebf3a
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-27 13:39:52 +0600

    medical history reducer added

---
+++ src/reducer/medicalHistoryReducer.js
@@ -0,0 +1,17 @@
+import { EpGetStorage, EpSetStorage, EpDefStorage } from '../utils/EpLocalStorage'
+
+//Set state as default storage value
+export const medicalHistoryState = EpDefStorage('medicalHistory', { medical: '' })
+
+export const medicalHistoryReducer = (state, action) => {
+    switch (action.type) {
+        case 'input':
+            EpSetStorage('medicalHistory', { medical: action.payload })
+            return JSON.parse(EpGetStorage('medicalHistory'))
+        case 'remove':
+            EpDefStorage('medicalHistory', { medical: '' })
+            return JSON.parse(EpGetStorage('medicalHistory'))
+        default:
+            return state
+    }
+}
