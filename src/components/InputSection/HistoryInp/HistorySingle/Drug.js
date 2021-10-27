2e609d4a9edbe9a9a15bce655e59c8d4f704a82f
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-27 13:18:19 +0600

    drug history input in localstorage

--- src/components/InputSection/HistoryInp/HistorySingle/Drug.js
+++ src/components/InputSection/HistoryInp/HistorySingle/Drug.js
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import Suggestion from '../../../ReUsable/Suggestion/Suggestion'
+import { useState, useContext } from 'react'
+import { History } from '../../../../allContext'
+// import Suggestion from '../../../ReUsable/Suggestion/Suggestion'
 import TextField from '../../../ReUsable/TextField/TextField'
 import classes from '../HistoryInp.module.css'
 
 const Drug = () => {
-    const [drug, setDrug] = useState('')
-    const arr = []
+    const [text, setText] = useState('')
+    const { stateDrugHistory, dispatchDrugHistory } = useContext(History)
+    // const arr = []
+
+    const submit = (e) => {
+        e.preventDefault()
+        dispatchDrugHistory({
+            type: 'input',
+            payload: stateDrugHistory.drug.length === 0 ? text : stateDrugHistory.drug.concat('\n' + text),
+        })
+        setText('')
+    }
     return (
         <div>
-            <TextField text={drug} setText={setDrug} label="Drug History" />
-            {drug.length !== 0 ? <Suggestion arr={arr} setText={setDrug} /> : null}
-            <button className={classes.btn}>Submit</button>
+            <TextField text={text} setText={setText} label="Drug History" />
+            {/* {drug.length !== 0 ? <Suggestion arr={arr} setText={setDrug} /> : null} */}
+            <button onClick={submit} className={classes.btn}>
+                Submit
+            </button>
         </div>
     )
 }
