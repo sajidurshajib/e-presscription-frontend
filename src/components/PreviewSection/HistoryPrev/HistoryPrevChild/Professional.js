eb8a85bcd5898483bc0ce09e61014d4dd61aa173
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-27 12:20:52 +0600

    professional history show from localstorage

--- src/components/PreviewSection/HistoryPrev/HistoryPrevChild/Professional.js
+++ src/components/PreviewSection/HistoryPrev/HistoryPrevChild/Professional.js
@@ -1,22 +1,33 @@
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
+import { useContext } from 'react/cjs/react.development'
+import { History } from '../../../../allContext'
 import classes from './HistoryPrevChild.module.css'
 
 const Professional = () => {
     const [toggle, setToggle] = useState(true)
+    const { stateProfessionalHistory } = useContext(History)
     return (
         <div className={classes.HistoryPrev}>
-            <h4
-                className={toggle ? classes.historyChildH : `${classes.historyChildH} ${classes.toggle}`}
-                onClick={() => setToggle(!toggle)}>
-                Professional history
-            </h4>
+            {stateProfessionalHistory.professional.length !== 0 ? (
+                <h4
+                    className={toggle ? classes.historyChildH : `${classes.historyChildH} ${classes.toggle}`}
+                    onClick={() => setToggle(!toggle)}>
+                    Professional history
+                </h4>
+            ) : null}
             {toggle ? (
-                <>
-                    <ul className={classes.historyli}>
-                        <li>Demo exist</li>
-                        <li>Demo problem</li>
-                    </ul>
-                </>
+                <Fragment>
+                    {stateProfessionalHistory.professional.length !== 0 ? (
+                        <ul className={classes.historyli}>
+                            {stateProfessionalHistory.professional
+                                .replace(/\n+$/, '')
+                                .split('\n')
+                                .map((v, i) => {
+                                    return <li key={i}>{v}</li>
+                                })}
+                        </ul>
+                    ) : null}
+                </Fragment>
             ) : null}
         </div>
     )
