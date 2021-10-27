5043c88da59f85a3f6906f59426b5943bbdc6cc1
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-27 12:49:20 +0600

    family history reducer added

---
+++ src/reducer/familyHistoryReducer.js
@@ -0,0 +1,17 @@
+import { EpGetStorage, EpSetStorage, EpDefStorage } from '../utils/EpLocalStorage'
+
+//Set state as default storage value
+export const familyHistoryState = EpDefStorage('familyHistory', { family: '' })
+
+export const familyHistoryReducer = (state, action) => {
+    switch (action.type) {
+        case 'input':
+            EpSetStorage('familyHistory', { family: action.payload })
+            return JSON.parse(EpGetStorage('familyHistory'))
+        case 'remove':
+            EpDefStorage('familyHistory', { family: '' })
+            return JSON.parse(EpGetStorage('familyHistory'))
+        default:
+            return state
+    }
+}
