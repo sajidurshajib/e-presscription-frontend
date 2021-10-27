13771000c0096b7c983953be39b4a54fb38fd76f
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-27 15:35:08 +0600

    vaccination history reducer added

---
+++ src/reducer/vaccinationHistoryReducer.js
@@ -0,0 +1,17 @@
+import { EpGetStorage, EpSetStorage, EpDefStorage } from '../utils/EpLocalStorage'
+
+//Set state as default storage value
+export const vaccinationHistoryState = EpDefStorage('vaccinationHistory', { vaccination: '' })
+
+export const vaccinationHistoryReducer = (state, action) => {
+    switch (action.type) {
+        case 'input':
+            EpSetStorage('vaccinationHistory', { vaccination: action.payload })
+            return JSON.parse(EpGetStorage('vaccinationHistory'))
+        case 'remove':
+            EpDefStorage('vaccinationHistory', { vaccination: '' })
+            return JSON.parse(EpGetStorage('vaccinationHistory'))
+        default:
+            return state
+    }
+}
