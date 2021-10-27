8c43ed93783c6f2a58ab14d2d4e95ce6d850b8c2
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-27 12:19:31 +0600

    professional history reducer added

---
+++ src/reducer/professionalHistoryReducer.js
@@ -0,0 +1,17 @@
+import { EpGetStorage, EpSetStorage, EpDefStorage } from '../utils/EpLocalStorage'
+
+//Set state as default storage value
+export const professionallHistoryState = EpDefStorage('professionalHistory', { professional: '' })
+
+export const professionalHistoryReducer = (state, action) => {
+    switch (action.type) {
+        case 'input':
+            EpSetStorage('professionalHistory', { professional: action.payload })
+            return JSON.parse(EpGetStorage('professionalHistory'))
+        case 'remove':
+            EpDefStorage('professionalHistory', { professional: '' })
+            return JSON.parse(EpGetStorage('professionalHistory'))
+        default:
+            return state
+    }
+}
