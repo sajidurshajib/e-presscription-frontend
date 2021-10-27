11b142df428fc13adf322b5223d69098e348197e
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-27 12:20:30 +0600

    professional historu input in localstorage

--- src/components/InputSection/HistoryInp/HistorySingle/Professional.js
+++ src/components/InputSection/HistoryInp/HistorySingle/Professional.js
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import Suggestion from '../../../ReUsable/Suggestion/Suggestion'
+import { useState, useContext } from 'react'
+import { History } from '../../../../allContext'
+// import Suggestion from '../../../ReUsable/Suggestion/Suggestion'
 import TextField from '../../../ReUsable/TextField/TextField'
 import classes from '../HistoryInp.module.css'
 
 const Professional = () => {
-    const [professional, setProfessional] = useState('')
-    const arr = []
+    const [text, setText] = useState('')
+    const { stateProfessionalHistory, dispatchProfessionalHistory } = useContext(History)
+    // const arr = []
+
+    const submit = (e) => {
+        e.preventDefault()
+        dispatchProfessionalHistory({
+            type: 'input',
+            payload:
+                stateProfessionalHistory.professional.length === 0
+                    ? text
+                    : stateProfessionalHistory.professional.concat('\n' + text),
+        })
+        setText('')
+    }
+
     return (
         <div>
-            <TextField text={professional} setText={setProfessional} label="Personal History" />
-            {professional.length !== 0 ? <Suggestion arr={arr} setText={setProfessional} /> : null}
-            <button className={classes.btn}>Submit</button>
+            <TextField text={text} setText={setText} label="Personal History" />
+            {/* {professional.length !== 0 ? <Suggestion arr={arr} setText={setProfessional} /> : null} */}
+            <button onClick={submit} className={classes.btn}>
+                Submit
+            </button>
         </div>
     )
 }
