9194d700ce065240b501f4d7e8d8e1dc6b6f08ec
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-27 15:39:08 +0600

    vaccination input in localstorage

--- src/components/InputSection/HistoryInp/HistorySingle/Vaccination.js
+++ src/components/InputSection/HistoryInp/HistorySingle/Vaccination.js
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import Suggestion from '../../../ReUsable/Suggestion/Suggestion'
+import { useState, useContext } from 'react'
+import { History } from '../../../../allContext'
+// import Suggestion from '../../../ReUsable/Suggestion/Suggestion'
 import TextField from '../../../ReUsable/TextField/TextField'
 import classes from '../HistoryInp.module.css'
 
 const Vaccination = () => {
-    const [vaccination, setVaccination] = useState('')
-    const arr = []
+    const [text, setText] = useState('')
+    const { stateVaccinationHistory, dispatchVaccinationHistory } = useContext(History)
+    // const arr = []
+    const submit = (e) => {
+        e.preventDefault()
+        dispatchVaccinationHistory({
+            type: 'input',
+            payload:
+                stateVaccinationHistory.vaccination.length === 0
+                    ? text
+                    : stateVaccinationHistory.vaccination.concat('\n' + text),
+        })
+        setText('')
+    }
     return (
         <div>
-            <TextField text={vaccination} setText={setVaccination} label="Vaccination History" />
-            {vaccination.length !== 0 ? <Suggestion arr={arr} setText={setVaccination} /> : null}
-            <button className={classes.btn}>Submit</button>
+            <TextField text={text} setText={setText} label="Vaccination History" />
+            {/* {vaccination.length !== 0 ? <Suggestion arr={arr} setText={setVaccination} /> : null} */}
+            <button onClick={submit} className={classes.btn}>
+                Submit
+            </button>
         </div>
     )
 }
