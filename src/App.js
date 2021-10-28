3ab013ffc14bd243ff6bb74c8da5569d0e5249ff
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-28 17:21:38 +0600

    Router removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import EprescriptionPage from './pages/EprescriptionPage'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
 
 const App = () => {
     return (
@@ -11,12 +9,6 @@ const App = () => {
                 <Switch>
                     <Route exact={true} path="/">
                         <EprescriptionPage />
-                    </Route>
-                    <Route path="/login">
-                        <LoginPage />
-                    </Route>
-                    <Route path="/register">
-                        <RegisterPage />
                     </Route>
                 </Switch>
             </Router>
