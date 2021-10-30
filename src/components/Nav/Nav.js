1f6b6fd9270764e3b5f3743b2a5ed35340a00e00
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-30 16:51:52 +0600

    Nav modified

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,11 @@
-import { faAlignLeft, faPrescription } from '@fortawesome/free-solid-svg-icons'
+import { faAlignLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useContext } from 'react'
+import { useContext, useReducer } from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import { SidebarContext } from '../../allContext'
+import { SidebarContext, PatientInfo } from '../../allContext'
 import epLogo from '../../assets/img/logo.png'
+import { patientState, patientReducer } from '../../reducer/PatientInfoReducer'
+import Patient from '../Patient/Patient'
 import classes from './Nav.module.css'
 
 const Nav = () => {
@@ -12,6 +14,7 @@ const Nav = () => {
     const { state, dispatch } = useContext(SidebarContext)
     let h = state.expand ? 'hide' : 'unhide'
 
+    const [statePatient, dispatchPatient] = useReducer(patientReducer, patientState)
     return (
         <div className={classes.Nav}>
             {/* e-prescription logo */}
@@ -24,15 +27,17 @@ const Nav = () => {
                     <img src={epLogo} alt="" />
                 </Link>
             )}
-
-            <ul>
+            <PatientInfo.Provider value={{ statePatient, dispatchPatient }}>
+                <Patient />
+            </PatientInfo.Provider>
+            {/* <ul>
                 <li>
                     <Link to="/">
                         <FontAwesomeIcon icon={faPrescription} />
                         ePrescription
                     </Link>
                 </li>
-            </ul>
+            </ul> */}
         </div>
     )
 }
