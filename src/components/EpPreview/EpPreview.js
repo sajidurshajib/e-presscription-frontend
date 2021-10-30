7a2bc0a13db0d35040443671309c18b019a0d778
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-30 17:42:33 +0600

    ePrescription preview page added

---
+++ src/components/EpPreview/EpPreview.js
@@ -0,0 +1,13 @@
+import classes from './EpPreview.module.css'
+
+const EpPreview = () => {
+    return (
+        <div className={classes.EpPreview}>
+            <div className={classes.Page}>
+                <h2>E prescription</h2>
+            </div>
+        </div>
+    )
+}
+
+export default EpPreview
