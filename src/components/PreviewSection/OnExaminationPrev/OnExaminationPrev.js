eb95f26c15aac2baed793f5d0bd45fa3ef49f586
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-31 15:20:17 +0600

    OnExamination Preview

--- src/components/PreviewSection/OnExaminationPrev/OnExaminationPrev.js
+++ src/components/PreviewSection/OnExaminationPrev/OnExaminationPrev.js
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-//useContext
-// import { OnExamination } from '../../../allContext'
+import { useState, Fragment, useContext } from 'react'
+import { OnExamination } from '../../../allContext'
 import classes from './OnExaminationPrev.module.css'
 
 const OnExaminationPrev = () => {
     const [toggle, setToggle] = useState(false)
-    // const { stateOnExamination } = useContext(OnExamination)
+    const { stateOnExamination } = useContext(OnExamination)
+
+    console.log(stateOnExamination)
+
     return (
         <div className={classes.OnExaminationPrev}>
             <h3 onClick={() => setToggle(!toggle)} className={toggle ? classes.toggle : null}>
                 On-Examination
             </h3>
             {toggle ? (
-                <>
+                <Fragment>
                     <ul>
                         <li>
                             BP <span>: 83</span>
@@ -24,7 +26,7 @@ const OnExaminationPrev = () => {
                             Temp <span>: 98</span>
                         </li>
                     </ul>
-                </>
+                </Fragment>
             ) : null}
         </div>
     )
