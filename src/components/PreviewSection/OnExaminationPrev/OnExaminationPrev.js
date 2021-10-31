3c6e6fc5bb384ef802ae0931dc31b578ed3e8902
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-31 16:10:25 +0600

    Preview pending

--- src/components/PreviewSection/OnExaminationPrev/OnExaminationPrev.js
+++ src/components/PreviewSection/OnExaminationPrev/OnExaminationPrev.js
@@ -1,21 +1,27 @@
 import { useState, Fragment, useContext } from 'react'
 import { OnExamination } from '../../../allContext'
+import { undef } from '../../../utils/TypeCheck'
 import classes from './OnExaminationPrev.module.css'
 
 const OnExaminationPrev = () => {
     const [toggle, setToggle] = useState(false)
     const { stateOnExamination } = useContext(OnExamination)
 
-    console.log(stateOnExamination)
-
     return (
         <div className={classes.OnExaminationPrev}>
-            <h3 onClick={() => setToggle(!toggle)} className={toggle ? classes.toggle : null}>
-                On-Examination
-            </h3>
+            {stateOnExamination.onexam.length !== 0 ? (
+                <h3 onClick={() => setToggle(!toggle)} className={toggle ? classes.toggle : null}>
+                    On-Examination
+                </h3>
+            ) : null}
             {toggle ? (
                 <Fragment>
                     <ul>
+                        {undef(stateOnExamination.onexam.bloodPressure.systolic) ? (
+                            <li>
+                                BP <span>{stateOnExamination.onexam.bloodPressure.systolic}</span>
+                            </li>
+                        ) : null}
                         <li>
                             BP <span>: 83</span>
                         </li>
