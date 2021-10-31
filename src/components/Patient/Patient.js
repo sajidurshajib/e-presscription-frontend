f8f7585f45d227f769ea4e5a663d712b85514740
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-31 15:19:30 +0600

    Patient style completed

--- src/components/Patient/Patient.js
+++ src/components/Patient/Patient.js
@@ -8,7 +8,7 @@ const Patient = () => {
     const [name, setName] = useState(statePatient.patient.name || '')
     const [phone, setPhone] = useState(statePatient.patient.phone || '')
     const [sex, setSex] = useState(statePatient.patient.sex || 'male')
-    const [age, setAge] = useState(statePatient.patient.age || 0)
+    const [age, setAge] = useState(statePatient.patient.age || '')
     const [address, setAddress] = useState(statePatient.patient.address || '')
 
     useEffect(() => {
@@ -28,19 +28,19 @@ const Patient = () => {
         <div className={classes.Patient}>
             <div className={classes.wrap1}>
                 <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Patient name" />
+                <input value={age} onChange={(e) => setAge(parseInt(e.target.value))} type="number" placeholder="Age" />
+                <select value={sex} onChange={(e) => setSex(e.target.value)}>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+            </div>
+            <div className={classes.wrap2}>
                 <input
                     value={phone}
                     onChange={(e) => setPhone(e.target.value)}
                     type="text"
                     placeholder="Phone number"
                 />
-            </div>
-            <div className={classes.wrap2}>
-                <select value={sex} onChange={(e) => setSex(e.target.value)}>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                </select>
-                <input value={age} onChange={(e) => setAge(parseInt(e.target.value))} type="number" placeholder="Age" />
                 <input value={address} onChange={(e) => setAddress(e.target.value)} type="text" placeholder="Address" />
             </div>
         </div>
