dfab715cacfe02fed30aa040de593c46ebe05d89
Sajidur Rahman <sajidur.inbox@gmail.com>
2022-01-12 15:59:46 +0600

    route: route env setup

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -6,15 +6,13 @@ import { Auth } from '../allContext'
 const ProtectedRoute = ({ component: Component, ...rest }) => {
     const { stateAuth } = useContext(Auth)
 
+    const api = process.env.NODE_ENV === 'production' ? process.env.DOC_SITE : env.DOC_SITE
+
     return (
         <Route
             {...rest}
             render={(props) =>
-                stateAuth.token.length !== 0 ? (
-                    <Component {...rest} {...props} />
-                ) : (
-                    (window.location.href = `${env.DOC_SITE}`)
-                )
+                stateAuth.token.length !== 0 ? <Component {...rest} {...props} /> : window.location.remove(`${api}`)
             }
         />
     )
