a77a417e7b878e305ece30ba6f3637b8cee4becf
sajidurshajib <sajidur.inbox@gmail.com>
2022-02-19 14:22:30 +0600

    patient year month added

--- src/components/Patient/Patient.js
+++ src/components/Patient/Patient.js
@@ -8,7 +8,9 @@ const Patient = () => {
     const [name, setName] = useState(statePatient.patient.name || '')
     const [phone, setPhone] = useState(statePatient.patient.phone || '')
     const [sex, setSex] = useState(statePatient.patient.sex || 'not selected')
-    const [age, setAge] = useState(statePatient.patient.age || '')
+
+    const [year, setYear] = useState(statePatient.patient.year || '')
+    const [month, setMonth] = useState(statePatient.patient.month || '')
     const [address, setAddress] = useState(statePatient.patient.address || '')
 
     useEffect(() => {
@@ -16,19 +18,37 @@ const Patient = () => {
             type: 'input',
             payload: {
                 name,
+                year,
+                month,
                 phone,
                 sex,
-                age,
                 address,
             },
         })
-    }, [name, phone, sex, age, address, dispatchPatient])
+    }, [name, phone, sex, year, month, address, dispatchPatient])
 
     return (
         <div className={classes.Patient}>
             <div className={classes.wrap1}>
                 <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Patient name" />
-                <input value={age} onChange={(e) => setAge(parseInt(e.target.value))} type="number" placeholder="Age" />
+                {/* <input value={age} onChange={(e) => setAge(parseInt(e.target.value))} type="number" placeholder="Age" /> */}
+                <label>Age: </label>
+                <input
+                    type="number"
+                    placeholder="Years"
+                    min={0}
+                    value={year}
+                    onChange={(e) => setYear(parseInt(e.target.value))}
+                />
+                <input
+                    type="number"
+                    placeholder="Months"
+                    min={0}
+                    value={month}
+                    onChange={(e) => setMonth(parseInt(e.target.value))}
+                />
+            </div>
+            <div className={classes.wrap2}>
                 <select value={sex} onChange={(e) => setSex(e.target.value)}>
                     <option value="not selected" disabled>
                         Sex
@@ -36,8 +56,6 @@ const Patient = () => {
                     <option value="male">Male</option>
                     <option value="female">Female</option>
                 </select>
-            </div>
-            <div className={classes.wrap2}>
                 <input
                     value={phone}
                     onChange={(e) => setPhone(e.target.value)}
