b05385c8001d9acbdcb52043a74480c1dc9b7bf1
sajidurshajib <sajidur.inbox@gmail.com>
2022-09-10 12:43:03 +0600

    refer ui done

--- src/components/InputSection/ReferInp/ReferInp.js
+++ src/components/InputSection/ReferInp/ReferInp.js
@@ -1,7 +1,13 @@
+import { useState } from 'react'
+import TextField from '../../ReUsable/TextField/TextField'
+import classes from './Refer.module.css'
+
 const ReferInp = () => {
+    const [refer, setRefer] = useState('')
     return (
-        <div>
-            <h2>Refer</h2>
+        <div className={classes.Refer}>
+            <TextField label={'Refer'} text={refer} setText={setRefer} />
+            <button>Submit</button>
         </div>
     )
 }
